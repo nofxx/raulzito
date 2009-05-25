b611feb4de79a145a0f287c9065ae2907b846cf7
Marcos Piccinini <x@nofxx.com>
2009-05-25 04:12:58 -0300

    rails 2.3

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -2,9 +2,9 @@ class MatchController < ApplicationController
   include MatchHelper
 
   before_filter :authorize
-  
+
   # GET /match/1
-  def show    
+  def show
     respond_to do |format|
       format.fbml # should be same as html
       format.html { render :template => 'match/result' and return if match.active == 0 }
@@ -13,18 +13,18 @@ class MatchController < ApplicationController
     end
   end
 
-  # GET /match/ 
+  # GET /match/
   def index
     # shows active matches
     @matches = current_player.matches.active
   end
 
-  # match/:id/status?move=N returns javascript to update the board to move N. 
+  # match/:id/status?move=N returns javascript to update the board to move N.
   # If no moves have been made and you query status for move 1 - it will not return
   # javascript to update the board, and will 304 after the first request. Once that
   # move has been made, though, future requests will return JS to update the board
   # and the URL that the client polls for.
-  def status 
+  def status
   end
 
   # provides the js of previous boards
@@ -52,10 +52,10 @@ class MatchController < ApplicationController
       attrs = {:black => current_player, :white => Player.find( params[:opponent_id] )}
     end
     setup = params[:start_pos]
-    
+
     attrs[:start_pos] = setup if setup and Fen::is_fen?( setup )
     @match = Match.create( attrs )
-    
+
     if setup and PGN::is_pgn?( setup )
       pgn = PGN.new( setup )
       pgn.playback_against( @match )
@@ -65,10 +65,10 @@ class MatchController < ApplicationController
     redirect_to match_url(@match.id) if @match
   end
 
-  private 
+  private
 
   def match
-    @match ||= if params[:id] 
+    @match ||= if params[:id]
       params[:id].to_i != 0 ? Match.find( params[:id] ) : Match.find_by_name( params[:id] )
     else
       Match.new # params[:match]?
