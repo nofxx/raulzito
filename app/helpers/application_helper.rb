42152fba880441bfe3009fe4cee2fdeba6d6f781
Marcos Piccinini <x@nofxx.com>
2009-05-25 07:50:33 -0300

    layout sem vergonha

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def display_flashes
+    flashes = ''
+    unless flash.size == 0
+      flash.each_pair  do |key, value|
+        flashes += content_tag(:div, content_tag(:span, value), :class => 'flashes '+key.to_s)
+      end
+    end
+    flashes
+  end
+
 end
