928071c6d6af5bbd03f92dee3966b66dae3f956f
escobera <rbocosta@gmail.com>
2009-05-25 05:48:55 -0300

    tabuleiro e peças que movem

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+    $('#board1').chess({fen : "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"});
+    $('[id^=board1_]').draggable();
+});
+
+
+
+// peça -> .mouseDown(validadePiece), .mouseUp(move(); if(validadeMove()) registerMove; push no outro jogador;)
